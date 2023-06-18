49d59ce3c5f967bd36dc3d150130419522ef90ff
Kawium <43068452+Kawium@users.noreply.github.com>
2023-06-18 22:45:27 +0200

    last

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
 const roomName = document.getElementById("room-name");
 const userList = document.getElementById("users");
+const locationBtn = document.getElementById("location-btn");
 
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
